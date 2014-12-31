f92d729e201c081071d8bed3a51424a624a2983c
rch850 <rich850@gmail.com>
2015-01-01 00:17:53 +0900

    draw line of packets.

--- src/popup.js
+++ src/popup.js
@@ -1,8 +1,8 @@
 $(function() {
   function initialize() {
     var mapOptions = {
-      zoom: 1,
-      center: new google.maps.LatLng(0, 0),
+      zoom: 3,
+      center: new google.maps.LatLng(35.7315015,139.7314098,13),
       mapTypeId: google.maps.MapTypeId.TERRAIN
     };
 
@@ -10,28 +10,27 @@ $(function() {
       mapOptions);
 
     var iplogs = chrome.extension.getBackgroundPage().iplogs;
-    var ipAddrCoordinates = [
-      new google.maps.LatLng(37.772323, -122.214897),
-      new google.maps.LatLng(21.291982, -157.821856),
-      new google.maps.LatLng(-18.142599, 178.431),
-      new google.maps.LatLng(-27.46758, 153.027892)
-    ];
 
-    iplogs.forEach(function(d) {
-      // TODO: push ipaddrcoordinates
-      $("body").append(d.timeStamp + " : " + d.ip + "<br>");
+    iplogs.forEach(function(d, index) {
+      console.log(d.timeStamp + " : " + d.ip);
+      $.get("http://ipinfo.io/" + d.ip + "/geo", function(response) {
+        var latlng = JSON.parse(response).loc.split(",");
+        var ipAddrCoordinates = [
+          new google.maps.LatLng(35.7315015,139.7314098,13),
+          new google.maps.LatLng(latlng[0], latlng[1])
+        ];
+        var packetPath = new google.maps.Polyline({
+          path: ipAddrCoordinates,
+          geodesic: true,
+          strokeColor: '#FF0000',
+          strokeOpacity: 1.0,
+          strokeWeight: 2
+        });
+
+        packetPath.setMap(map);
+      }, "jsonp");
+
     });
-
-    var packetPath = new google.maps.Polyline({
-      path: ipAddrCoordinates,
-      geodesic: true,
-      strokeColor: '#FF0000',
-      strokeOpacity: 1.0,
-      strokeWeight: 2
-    });
-
-    packetPath.setMap(map);
-
   }
 
   google.maps.event.addDomListener(window, 'load', initialize);
